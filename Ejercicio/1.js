1ab67c02ed1332d151240512f4ec1beebb1d9ddf
Jefferson <jeffersonhidalgo853@gmai.com>
2020-04-23 18:10:25 -0400

    comentario a partir de la modificacion

--- Ejercicio/1.js
+++ Ejercicio/1.js
@@ -3,7 +3,7 @@ let numero1=prompt("ingrese primer valor");
 let numero2=prompt("ingrese segundo valor");
 
 resto= numero1%numero2;
-function samuel(resto){
+function sJefferson(resto){
 
 if(resto==0){
     alert(numero1+"_es multiplo de_"+ numero2)
@@ -13,6 +13,6 @@ else{
     alert(numero1+"_no es multiplo de_"+numero2)
 }
 }
-alert (samuel(resto))
+alert (Jefferson(resto))
 Ç
 
